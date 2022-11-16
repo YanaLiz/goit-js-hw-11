339dd697d3eb4d4df757932bd6fb48e547fa1d37
YanaLiz <105737326+YanaLiz@users.noreply.github.com>
2022-11-16 19:52:03 +0200

    add

--- src/index.js
+++ src/index.js
@@ -1,23 +1,62 @@
 import axios from "axios";
 import NewsApiService from "./news-service";
+// import Notiflix from "notiflix";
+
 
 
 const refs = {
-    searchForm: document.querySelector('#search-form'),
-    galleryContainer:document.querySelector('.gallery'),
+searchForm: document.querySelector('#search-form'),
+loadMoreBtn:document.querySelector('.load-more'),
+articlesContainer: document.querySelector('.gallery'),
 }
 
 const newsApiService = new NewsApiService();
 
-
+console.log(newsApiService);
 
 refs.searchForm.addEventListener('submit', onSearch);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
 
 function onSearch(e) {
-    e.preventDefault()
+e.preventDefault();
 
-    newsApiService.query = e.currentTarget.elements.searchQuery.value;
-    newsApiService.fetchArticles();
+newsApiService.query = e.currentTarget.elements.searchQuery.value;
+newsApiService.resetPage();
+newsApiService.fetchArticles().then(appendArticlesMarkup);
 
 }
 
+function createArticlesMarkup(articlesMarkup) {
+return articlesMarkup
+.map(({ }) => {
+return `<div class="photo-card">
+    <img src="" alt="" loading="lazy" />
+    <div class="info">
+        <p class="info-item">
+            <b>${Likes}</b>
+        </p>
+        <p class="info-item">
+            <b>${Views}</b>
+        </p>
+        <p class="info-item">
+            <b>${Comments}</b>
+        </p>
+        <p class="info-item">
+            <b>${Downloads}</b>
+        </p>
+    </div>
+</div>`
+}).join('');
+}
+
+function onLoadMore() {
+newsApiService.fetchArticles().then(articles => console.log(articles));
+}
+
+function appendArticlesMarkup(articles) {
+refs.articlesContainer.insertAdjacentHTML('beforeend',createArticlesMarkup);
+}
+
+// function clearArticlesConainer() {
+// refs.articlesContainer.innerHTML = '';
+// }
--- src/news-service.js
+++ src/news-service.js
@@ -1,31 +1,43 @@
 export default class NewsApiService{
-    constructor() {
-        this.searchQuery = '';
-        this.page=1
-    }
-
-    fetchArticles() {
-        console.log(this)
-         const options = {
-    key: '31272833-6208e6f151d79070e75270c69'
+constructor() {
+this.searchQuery = '';
+this.page = 1;
 }
 
-const url=`https://pixabay.com/api/?key=31272833-6208e6f151d79070e75270c69&q=${this.searchQuery}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${this.page}`
+fetchArticles() {
+const options = {
+key: '31272833-6208e6f151d79070e75270c69'
+}
 
-fetch(url, options)
-    .then(r => r.json())
-    .then(data => {
-        this.page += 1;
-    });
-    }
+const
+url=`https://pixabay.com/api/?key=31272833-6208e6f151d79070e75270c69&page=${this.page}&q=${this.searchQuery}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40`
 
+return fetch(url, options)
+.then(r => r.json())
+.then(data => {
 
-    get query(){
-    return this.searchQuery;
+this.incrementPage();
+return data.articles;
+})
+.catch(error => {
+Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+});
+}
+
+incrementPage() {
+this.page += 1;
+}
+
+resetPage() {
+this.page = 1;
+}
+
+get query(){
+return this.searchQuery;
 }
 
 set query(newQuery){
-    this.searchQuery = newQuery;
+this.searchQuery = newQuery;
 }
 }
 
